241b054d0bb7e59340a0337d0c3ad96454b858e1
khangarid <khangarid.b@itc.gov.mn>
2020-11-04 16:41:39 +0800

    refactored login screeng

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,9 +2,8 @@ module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
   useTabs: false,
   printWidth: 120,
   semi: true,
-  endOfLine: 'auto'
+  endOfLine: 'auto',
 };
--- src/screens/Welcome/LoginScreen.tsx
+++ src/screens/Welcome/LoginScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useForm } from 'react-hook-form';
+import { Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Button from '../../common/Button';
 import DumbTextInput from '../../common/TextInput';
 
@@ -8,32 +9,48 @@ interface IProps {
 }
 
 const LoginScreen = (props: IProps) => {
+  const { handleSubmit, register, setValue, formState } = useForm({ mode: 'onChange' });
+
+  React.useEffect(() => {
+    register({ name: 'username' }, { required: true });
+    register({ name: 'password' }, { required: true });
+  }, [register]);
+
+  const onSubmit = () => {
+    console.log('submitted');
+  };
+
   return (
-    <View style={{ justifyContent: 'space-between', height: '100%', backgroundColor: 'white' }}>
-      <View style={{ padding: 20 }}>
-        <View>
-          <Text style={styles.headerText}>Lorem Ipsum</Text>
-          <Text style={styles.headerText}>Lorem Ipsum is</Text>
+    <SafeAreaView style={{ backgroundColor: 'white', height: '100%' }}>
+      <ScrollView>
+        <View style={{ padding: 20, flex: 1 }}>
+          <View>
+            <Text style={styles.headerText}>Lorem Ipsum</Text>
+            <Text style={styles.headerText}>Lorem Ipsum is</Text>
+          </View>
+
+          <View style={{ marginTop: 30 }}>
+            <DumbTextInput fullWidth placeholder="Username" onChangeText={(text) => setValue('username', text)} />
+            {formState.errors.username && <Text style={{ color: 'red' }}>* Please enter username</Text>}
+
+            <View style={{ marginTop: 5 }}>
+              <DumbTextInput fullWidth placeholder="Password" onChangeText={(text) => setValue('password', text)} />
+              {formState.errors.password && <Text style={{ color: 'red' }}>* Please enter password</Text>}
+            </View>
+
+            <View style={{ marginTop: 15 }}>
+              <Button text="Login" fullWidth onPress={handleSubmit(onSubmit)} />
+            </View>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'center', width: '100%', marginTop: 15 }}>
+              <Text>Forgot password?</Text>
+              <Pressable onPress={() => props.navigation.navigate('ForgotPassword')}>
+                <Text style={{ color: 'green', textDecorationLine: 'underline' }}> Click here</Text>
+              </Pressable>
+            </View>
+          </View>
         </View>
-
-        <View style={{ marginTop: 30 }}>
-          <DumbTextInput fullWidth placeholder="Username" />
-
-          <View style={{ marginTop: 5 }}>
-            <DumbTextInput fullWidth placeholder="Password" />
-          </View>
-          <View style={{ marginTop: 15 }}>
-            <Button disabled text="Login" fullWidth />
-          </View>
-
-          <View style={{ flexDirection: 'row', justifyContent: 'center', width: '100%', marginTop: 15 }}>
-            <Text>Forgot password?</Text>
-            <Pressable onPress={() => props.navigation.navigate('ForgotPassword')}>
-              <Text style={{ color: 'green', textDecorationLine: 'underline' }}> Click here</Text>
-            </Pressable>
-          </View>
-        </View>
-      </View>
+      </ScrollView>
 
       <View
         style={{
@@ -43,13 +60,14 @@ const LoginScreen = (props: IProps) => {
           borderTopWidth: 1,
           flexDirection: 'row',
           justifyContent: 'center',
+          width: '100%',
         }}>
         <Text>Don't have an account?</Text>
         <Pressable onPress={() => props.navigation.navigate('Register')}>
           <Text style={{ color: 'green', textDecorationLine: 'underline' }}> Register here</Text>
         </Pressable>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
